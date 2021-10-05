4875c200e3692107a13a8a33b0455ed100f22d4d
Juan <jerabelo@miners.utep.edu>
2021-10-04 19:47:57 -0600

    updated uimain.c

--- src/uimain.c
+++ src/uimain.c
@@ -5,8 +5,32 @@
 #include <string.h>
 #include <ctype.h>
 
+#define LENGTH 101
 int main()
 {
-  
-}
+  char userIn[LENGTH];
+  userIn[100] = '\0';
+  List* history = init_history();
 
+  printf("\nEnter string. (h) to see history. (!<num>) to access specific history. (q) to quit.");
+
+  char** tokens;
+  while(1) {
+      printf("%s", "> ");
+      fgets(userIn,100,stdin);
+      if(userIn[0] == '/' && userIn[1] == 'q' && userIn[2] == 'u' && userIn[3] == 'i' && userIn[4] == 't'){
+	return 0;
+      } else if(userIn[0] == '/') {
+	if(userIn[1] == '\n') {
+	  printf("%s\n", get_history(history,userIn[1] - '0'));
+	} else {
+	  print_history(history);
+	}
+      } else {
+	add_history(history, userIn);
+	tokens = tokenize(userIn);
+	printf("Number of words: %d\n", count_words(userIn));
+	print_tokens(tokens);
+      }
+  }
+}
